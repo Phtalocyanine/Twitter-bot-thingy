98ab1fe189aa2eb97834157b34c61f846a261444
Phtalocyanine <70093238+Phtalocyanine@users.noreply.github.com>
2021-08-17 09:47:35 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  consumer_key:         'm3wJBv3OLNHVKsmuzom1jFPvs',
+  consumer_secret:      '7chJBonDSkBAWtnOrmsH0Jg44kK9155JNxytDmKsi6JRamMRp9',
+  access_token:         '1348066546464251911-cErIcDozYMO8obgGXvzovDIcjUyLPG',
+  access_token_secret:  'xaq3gbzjbZWFfOLxTYoHuLGLfnyUsffqm3czfAxujYhGt'
+}
+
+module.exports = config;
